ad6d93ebb49873f62bd63e470f545b606cc36385
Vladimir <mrpol05pol@gmail.com>
2016-10-26 18:24:05 +0300

    Add files via upload
    26.10.2016

---
+++ 23-27.php
@@ -0,0 +1,27 @@
+﻿<?php
+//<p>23. Напишите 3 строки, используя три разные функции для вывода текста.</p>
+echo 'занятие 23 <br>';
+echo'hello <br>';
+print'hello <br>';
+$a = 'hello';
+print_r($a).'<br>';
+
+//<p>24. Создайте в файле 3 комментария тремя разными типами.</p>
+echo 'занятие 24 <br>';
+//echo 'занятие 23 <br>';
+//echo 'занятие 23 <br>.<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur, aut incidunt inventore minus nam non provident quod repudiandae voluptates. Eius, impedit ipsum nam nemo praesentium quia totam vitae voluptate.</p>>';
+
+
+//<p>25. Приведите пример, чем отличается &lt;?php от &lt;?=.</p>
+//<?php обычная форма оператора <?= сокращенной формой оператора, не на всех серверах работает, в файле php.ini нужно включать short_open_tag
+
+
+//<p>26. Объявите константу DAYS_COUNT равную 7 и константу MONTH_COUNT равную 12 двумя разными способами объявления констант.</p>
+//echo 'занятие 26 <br>';
+//define("DAYS_COUNT", 7);
+//define("MONTH_COUNT", 12)
+//echo 'DAYS_COUNT '.DAYS_COUNT.'MONTH_COUNT '.MONTH_COUNT.'<br>';
+//не правильно
+
+//<p>27. Выполните скрипт по задаче №4 из консоли (необходимо запустить php скрипт по задаче №4 из командной строки).</p>
+?>
